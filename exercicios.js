976d4766b2ccc7967384821a557013547657d950
Mylena Marques Ribeiro <ribeiro.mylena1@gmail.com>
2024-11-28 13:20:47 -0300

    Projeto lista de compras pronto

---
+++ exercicios.js
@@ -0,0 +1,9 @@
+cliente = {
+    nome: 'Mariana', 
+    idade: 35,
+    sexo: 'Feminino',
+}
+
+cliente.estado = 'Pernambuco';
+
+console.log(`Olá, meu nome é ${cliente.nome} e moro no estado de ${cliente.estado}`);
